7f00e60e9fd55afa736b4a3ee28c492e85c3167c
Neyan najat <nyannajat8@gmail.com>
2018-09-22 08:43:27 +0000

    automatically backed up by learn

--- src/components/SimpleComponent.js
+++ src/components/SimpleComponent.js
@@ -1,1 +1,23 @@
 // Code SimpleComponentHere Here
+import React from "react";
+export default class SimpleComponent extends React.Component 
+{
+  constructor(props)
+  {
+    super(props)
+    this.state = {
+      mood:"happy"
+    }
+  }
+  handleClick = () => {
+    this.setState({
+      mood:"sad"
+    })
+  }
+  render()
+  {
+    return (<div onClick = {() => this.handleClick()}>
+     {this.state.mood}
+    </div>)
+  }
+}
